5e0dc4760841c0e1a1e380ed0e5c9e91a0e1b7f7
abiyosa <abiyosapratama@gmail.com>
2022-03-10 11:32:03 +0700

    Convert HTML to svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/65535/17102022497_cb049e3eac_c.jpg',
+            pledged: 0,
+            target: 100000,
+            date_end: +new Date('10 August 2020'),
+            profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}
